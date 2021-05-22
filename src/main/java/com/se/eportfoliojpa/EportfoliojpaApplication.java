3b7b4237569ce5fec9054404c892bd85d802cf99
leonhardstengel <leonhard.stengel@outlook.de>
2021-05-22 17:18:36 +0200

    Initial commit

---
+++ src/main/java/com/se/eportfoliojpa/EportfoliojpaApplication.java
@@ -0,0 +1,13 @@
+package com.se.eportfoliojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EportfoliojpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EportfoliojpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se/eportfoliojpa/EportfoliojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.eportfoliojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EportfoliojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
